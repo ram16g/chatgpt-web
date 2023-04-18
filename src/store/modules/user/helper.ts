e2f15e6fead71976d8fde9608c5bcfea448c54be
liuke <anlianfengjie@hotmail.com>
2023-04-18 16:29:53 +0800

    去掉github仓库信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'ChatGPT',
+      description: '我是您的 AI 助手，快向我提问吧',
     },
   }
 }
